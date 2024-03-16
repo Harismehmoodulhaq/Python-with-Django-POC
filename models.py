f52978e36ab81d7ef33ece92fc0f62c928a938ed
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2024-03-16 17:43:14 +0300

    second commit

--- models.py
+++ models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class React(models.Model):
-    employee = models.CharField(max_Length=30)
-    department = models.CharField(max_Length=200)
--- views.py
+++ views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from . models import *
-# from . serializer import *
-# Create your views here.
 
-# class ReactView(APIView):
-    def get(self, request):
-        output = []
+# Create your views here.
